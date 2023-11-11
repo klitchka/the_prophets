de3d430d58b3706f4299b88afc813dca49cfaee2
ayushtom <tomar2585@gmail.com>
2023-11-11 18:51:40 +0530

    feat: add marketplace button

--- src/app/Woho.js
+++ src/app/Woho.js
@@ -1,13 +1,13 @@
 "use client";
 import { useCallback, useEffect, useState } from "react";
-import styles from "./styles/page.module.css";
+import styles from "./page.module.css";
 import Image from "next/image";
 import Connect from "./components/Connect";
 import OpenLogin from "@toruslabs/openlogin";
 import { useAccount, useConnect, useDisconnect } from "@starknet-react/core";
 import FootballGif from "../../public/football.gif";
 
-function Home() {
+function Woho() {
   const { connect, connectors } = useConnect();
   const { disconnect } = useDisconnect();
   const { address, isConnected } = useAccount();
@@ -49,19 +49,30 @@ function Home() {
   // ... código restante ...
 
   return (
-    <div className={styles.hero_container}>
-      <div className={styles.hero_child_container}>
-        <h1 className={styles.hero_heading}>Sports</h1>
-        <h1 className={styles.hero_heading}>Betting</h1>
-        <h1 className={styles.hero_heading}>Reimagined</h1>
+    <main className={styles.main}>
+      <div className={styles.navbar}>
+        <div>
+          <h3>Prophets</h3>
+        </div>
+        <div>
+          <button className={styles.connect_button}>Connect</button>
+        </div>
       </div>
-      <div
-        className={styles.hero_child_container}
-        style={{ alignItems: "center" }}
-      >
-        <Image src={FootballGif} priority className={styles.hero_image} />
+
+      <div className={styles.hero_container}>
+        <div className={styles.hero_child_container}>
+          <h1 className={styles.hero_heading}>Sports</h1>
+          <h1 className={styles.hero_heading}>Betting</h1>
+          <h1 className={styles.hero_heading}>Reimagined</h1>
+        </div>
+        <div
+          className={styles.hero_child_container}
+          style={{ alignItems: "center" }}
+        >
+          <Image src={FootballGif} priority className={styles.hero_image} />
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
 
