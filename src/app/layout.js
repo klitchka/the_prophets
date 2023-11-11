c1e36151fa3d34899d0951a9411af7e296db8971
Ana Aparicio <aaparici@student.42madrid.com>
2023-11-11 22:09:25 +0300

    logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import {StarknetProvider} from "./components/starknet-provider";
 import styles from "./styles/page.module.css";
 import Link from "next/link";
 import Connect from "./components/Connect";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,7 @@ export default function RootLayout({ children }) {
         <main className={styles.main}>
           <div className={styles.navbar}>
             <Link href="/">
-              <h3>Prophets</h3>
+              <Image src="/logo.png" alt="Logo" width={200} height={100} />
             </Link>
             <div style={{ display: "flex", gap: "2rem", alignItems: "center" }}>
               <Link href="/marketplace">
